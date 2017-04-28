f78ba7f6fba54a0fbf37aa42df9cb427f66f2631
s20041205 <stlinalice@gmail.com>
2017-04-28 20:46:24 +0800

    Add files via upload
    SBP decision revised.

--- dataRdWr.m
+++ dataRdWr.m
@@ -79,8 +79,51 @@ pkECG = pkECG';
 locpkECG = locpkECG';
  
 %% SBP decision
-[loc_pk_slopeMax, loc_sbp, pt_sbp] = sbpDect(pkPPGcuff, locpkPPGcuff, locpkPPGsen, prPPGsen, CuffDC, fs);
+% [loc_pk_slopeMax, loc_sbp, pt_sbp] = sbpDect(pkPPGcuff, locpkPPGcuff, locpkPPGsen, prPPGsen, CuffDC, fs);
 % [loc_pk_slopeMax, loc_sbp, pt_sbp] = sbpDect_v2(pkPPGcuff, locpkPPGcuff, locpkPPGsen, prPPGsen, CuffDC, fs);
+ppi_cuff = [];
+ppi_change = [];
+% ppg_vanish = [];
+ppg_appear = [];
+pt_sbp = [];
+ppi_temp = [];
+ppi_ct = 0;
+ppiCt_lim = 10;
+% Find pkPPGcuff after CuffDCmax: deflation start
+ct = 1;
+while locpkPPGcuff(ct) < 1000
+    ct = ct + 1;
+end
+ppi_temp = locpkPPGcuff(ct) - locpkPPGcuff(ct-1); % save refrence ppi
+% Start point
+for ct_slope = ct: length(pkPPGcuff(:,1))
+    if ((locpkPPGcuff(ct_slope)-locpkPPGcuff(ct_slope-1)) >= ppi_temp*1.5) && (pkPPGcuff(ct_slope) > 50) %&& (CuffDC(locpkPPGcuff(ct_slope))>=135)
+       if isempty(loc_pk_slopeMax)
+           loc_pk_slopeMax = ct_slope;
+           fprintf('Deflation start (%d): %0.2f\n', locpkPPGcuff(ct_slope), CuffDC(locpkPPGcuff(ct_slope)));
+       end
+    end
+end
+
+for i = loc_pk_slopeMax:length(locpkPPGcuff)
+    ppi_cuff = locpkPPGcuff(i) - locpkPPGcuff(i-1);
+    if isempty(pt_sbp)
+        if (ppi_cuff >= ppi_temp*0.8) && (ppi_cuff <= ppi_temp*1.2) && (CuffDC(locpkPPGcuff(i))<= CuffDC(locpkPPGcuff(loc_pk_slopeMax)))
+            ppi_ct = ppi_ct + 1;
+            ppg_appear = [ppg_appear; i]; 
+            if (ppi_ct > ppiCt_lim) && (ppi_ct-ppiCt_lim >= ppg_appear(end)-ppg_appear(end-ppiCt_lim))                    
+                for ct = 2:length(ppg_appear)
+                    if isempty(ppi_change) && (ppg_appear(ct)-ppg_appear(ct-1) == 1)
+                        ppi_change = [ppi_change; ppg_appear(ct-1)];
+
+                    pt_sbp = locpkPPGcuff(ppi_change(end)-1);
+                    fprintf('SBP found (%d): %0.2f\n', pt_sbp, CuffDC(pt_sbp));
+                    end
+                end               
+            end
+        end
+    end
+end
 
 %% PTT calculation
 [ptt] = PTTcalc(locpkECG, locpkPPGsen);
